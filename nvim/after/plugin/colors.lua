0ec092a116d7946735450dcd6b8047a4c046ff8f
Ali Yeysides <ali.yeysides@gmail.com>
2023-05-01 21:51:28 -0400

    updates opacity

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,14 @@
 function SetColors(color)
-  color = color or "github_dark"
+  color = color or "rose-pine"
   vim.cmd.colorscheme(color)
+  vim.g.rose_pine_variant = "moon"
+  vim.g.rose_pine_disable_background = true
+  vim.g.rose_pine_disable_italics = true
+  vim.g.rose_pine_disable_eob = true
+  vim.g.rose_pine_background = "transparent"
+  vim.g.rose_pine_borders = true
+  -- configure opacity of background 
+  vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 end
 
-SetColors("rose-pine")
+SetColors()
