c01475bc3944962e853c577adf05a81b06b71723
Ali Yeysides <ali.yeysides@gmail.com>
2023-05-17 19:35:50 -0400

    updates harpoon keymap

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,4 +1,4 @@
 local harpoon = require("harpoon")
 
-vim.api.nvim_set_keymap("n", "<leader>hs", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", { noremap = true, silent = true })
