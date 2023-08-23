2c9078fed56c09e5f3c78d1eb04bb6df97e7ff2e
Ali Yeysides <ali.yeysides@gmail.com>
2023-08-23 16:19:14 -0400

    updates telescope find files to include hidden files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,9 @@
 local builtin = require('telescope.builtin')
 local keymap = vim.keymap
 
-keymap.set('n', '<leader>ff', builtin.find_files, {})
+keymap.set('n', '<leader>ff', function()
+  builtin.find_files({ find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' } });
+end)
 keymap.set('n', '<leader>gt', builtin.git_files, {})
 keymap.set('n', '<leader>pf', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
--- nvim/lua/aliyeysides/plugins/telescope.lua
+++ nvim/lua/aliyeysides/plugins/telescope.lua
@@ -1,4 +1,7 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.2',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
 }
