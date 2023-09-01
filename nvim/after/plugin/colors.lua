f5cdf833319878ffbb69ba9531602ec7121aaf75
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-01 17:49:21 -0400

    cleanup

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -5,12 +5,6 @@ require('rose-pine').setup({
 function SetColors(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
-  vim.g.rose_pine_variant = "moon"
-  vim.g.rose_pine_disable_background = true
-  vim.g.rose_pine_disable_italics = true
-  vim.g.rose_pine_disable_eob = true
-  vim.g.rose_pine_background = "transparent"
-  vim.g.rose_pine_borders = true
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
