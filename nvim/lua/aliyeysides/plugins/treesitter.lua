277a5b1d725be35ab4ce4986e53fe46e334d1b0b
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-02 17:41:22 -0400

    removes redundant config in treesitter

--- nvim/lua/aliyeysides/plugins/treesitter.lua
+++ nvim/lua/aliyeysides/plugins/treesitter.lua
@@ -1,10 +1,4 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = "all",
-      highlight = { enable = true, }
-    }
-  end
 }
