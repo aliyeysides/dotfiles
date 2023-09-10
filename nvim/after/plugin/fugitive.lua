c00d00a7814828e38389bf25be2bb79a0b77706f
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-09 21:47:38 -0400

    updates fugitive git diff command

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
 
 map("n", "<leader>gs", ":Git<CR>")
-map("n", "<leader>gd", ":Gdiff<CR>")
+map("n", "<leader>h", ":Gdiff<CR>")
 map("n", "<leader>gc", ":Git commit<CR>")
