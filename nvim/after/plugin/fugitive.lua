b575805bdf103f606779d824015940c580fc16eb
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-24 11:25:10 -0400

    adds fugitive git push remap

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -3,3 +3,4 @@ local map = vim.keymap.set
 map("n", "<leader>gs", ":Git<CR>")
 map("n", "<leader>h", ":Gdiff<CR>")
 map("n", "<leader>gc", ":Git commit<CR>")
+map("n", "<leader>gp", ":Git push<CR>")
