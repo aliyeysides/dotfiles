47b1abb9dc7a7d8d322f2d88b4b22f2fc16af992
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-24 11:00:49 -0400

    adds autocmds for winbar on new win splits

--- nvim/lua/aliyeysides/init.lua
+++ nvim/lua/aliyeysides/init.lua
@@ -1,6 +1,9 @@
 require("aliyeysides.remap")
 require("aliyeysides.set")
 
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
 -- init lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -15,5 +18,27 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+
+local aliyeysidesgroup = augroup("aliyeysides", {})
+
+autocmd("WinNew", {
+  group = aliyeysidesgroup,
+  pattern = "*",
+  callback = function()
+    vim.opt.winbar = "%f"
+  end,
+})
+
+autocmd("WinClosed", {
+  group = aliyeysidesgroup,
+  pattern = "*",
+  callback = function()
+    local wincount = vim.api.nvim_list_wins()
+
+    if #wincount == 2 then -- netrw counts as a window I guess?
+      vim.opt.winbar = ""
+    end
+  end,
+})
 
 require("lazy").setup("aliyeysides.plugins")
