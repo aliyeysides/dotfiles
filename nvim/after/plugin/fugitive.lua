dc8dd112f39e5c04327142cc434927a56f830b7a
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-24 20:30:58 -0400

    adds git log fugitive remap

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -4,3 +4,5 @@ map("n", "<leader>gs", ":Git<CR>")
 map("n", "<leader>h", ":Gdiff<CR>")
 map("n", "<leader>gc", ":Git commit<CR>")
 map("n", "<leader>gp", ":Git push<CR>")
+map("n", "<leader>gl", ":Git log<CR>")
+
