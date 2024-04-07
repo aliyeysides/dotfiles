51751df83bcb9a395671d0cb7aeb887cb4bb614a
Ali Yeysides <ali.yeysides@gmail.com>
2024-04-06 21:07:23 -0700

    updates rose-pine config

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,14 @@
 require('rose-pine').setup({
+  disable_italics = true,
+  styles = {
+    bold = true,
+    italic = false,
+  },
   disable_background = true,
 })
 
 function SetColors(color)
-  color = color or "rose-pine"
+  color = color or "rose-pine-moon"
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
