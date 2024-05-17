695df1b57f33274a3be7d5d4d0fe9bc7d80167fa
Ali Yeysides <ali.yeysides@gmail.com>
2024-05-17 18:03:30 -0400

    adds oil plugin

---
+++ nvim/lua/aliyeysides/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup {
+      columns = { "icon" },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end
+}
