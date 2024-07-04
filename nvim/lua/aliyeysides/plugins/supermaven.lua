f44701fdadb50014681dc5a81f76d0b98a1dc1f3
Ali Yeysides <ali.yeysides@gmail.com>
2024-07-04 11:38:15 -0400

    adds supermaven ignored files

--- nvim/lua/aliyeysides/plugins/supermaven.lua
+++ nvim/lua/aliyeysides/plugins/supermaven.lua
@@ -2,6 +2,7 @@ return {
   "supermaven-inc/supermaven-nvim",
   config = function()
     require("supermaven-nvim").setup({
+      ignore_filetypes = { c = true },
       keymaps = {
         accept_suggestion = "<C-s>",
         accept_word = "<C-d>",
