3722f6f4d024b8f8aa98c61bd062b3f4deffdf2c
Ali Yeysides <ali.yeysides@gmail.com>
2024-09-27 14:27:22 -0400

    feat: increase the size of the harpoon window

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,8 +1,14 @@
-require("harpoon")
+local harpoon = require("harpoon")
 
 local map = vim.keymap.set
 
 map("n", "<leader>s", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-  { noremap = true, silent = true })
+    { noremap = true, silent = true })
 map("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>",
-  { noremap = true, silent = true })
+    { noremap = true, silent = true })
+
+harpoon.setup({
+    menu = {
+        width = vim.api.nvim_win_get_width(0) - 100,
+    }
+})
