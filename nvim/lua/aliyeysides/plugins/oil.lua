c677a9f0f9ef035d693d086c1a0b09fd249dc0b9
Ali Yeysides <ali.yeysides@gmail.com>
2024-10-13 06:07:48 -0400

    chore: display oil keymap

--- nvim/lua/aliyeysides/plugins/oil.lua
+++ nvim/lua/aliyeysides/plugins/oil.lua
@@ -1,16 +1,20 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup {
-      columns = { "icon" },
-      view_options = {
-        show_hidden = true,
-      },
-    }
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup {
+            columns = { "icon" },
+            view_options = {
+                show_hidden = true,
+            },
+            keymaps = {
+                ["<C-h>"] = false,
+                ["<C-l>"] = false,
+            }
+        }
 
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end
 }
