8dcd5779f7b14d96901e5940fe89a7aa25a4a24f
Ali Yeysides <ali.yeysides@gmail.com>
2024-10-15 10:37:42 -0400

    feat: fix go formating

--- nvim/lua/aliyeysides/init.lua
+++ nvim/lua/aliyeysides/init.lua
@@ -64,6 +64,17 @@ autocmd("bufWritePost", {
     command = "silent !black %",
 })
 
+-- Format Go code using tabs
+autocmd("FileType", {
+    pattern = "go",
+    callback = function()
+        vim.opt_local.expandtab = false
+        vim.opt_local.shiftwidth = 8
+        vim.opt_local.tabstop = 8
+        vim.opt_local.softtabstop = 8
+    end
+})
+
 -- Save file on W or w
 usercmd("W", "w", {})
 
